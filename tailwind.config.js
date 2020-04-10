f4966d69dd1608a7768bff269e13dcfeaaf5b461
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-04-10 16:41:54 +0200

    Add colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,35 @@
 module.exports = {
   theme: {
-    extend: {},
+    fontFamily: {
+      montserrat: ['Montserrat'],
+      'open-sans': ['Open Sans']
+    },
+    extend: {
+      colors: {
+        'tangerine': {
+          100: '#FFF6F1',
+          200: '#FFE7DB',
+          300: '#FFD9C5',
+          400: '#FFBD9A',
+          500: '#FFA06F',
+          600: '#E69064',
+          700: '#996043',
+          800: '#734832',
+          900: '#4D3021',
+        },
+        'swamp-green': {
+          100: '#F7F8F3',
+          200: '#EAEFE1',
+          300: '#DEE5CF',
+          400: '#C5D1AA',
+          500: '#ACBD86',
+          600: '#9BAA79',
+          700: '#677150',
+          800: '#4D553C',
+          900: '#343928',
+        },
+      }
+    },
   },
   variants: {},
   plugins: [],
