2040348166e592d3dfe39470265b75609c5cf3f4
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-04-13 18:08:11 +0200

    Update barrel

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,10 @@
 export { ProjectCards } from './ProjectCards';
 export { Header } from './Header';
 export { Nav } from './Nav';
+export { IconLink } from './IconLink';
+export { SkillsList } from './SkillsList';
+export { ProjectSection } from './ProjectsSection';
+export { AboutSection } from './AboutSection';
+export { SectionHeading } from './SectionHeading';
+export { IntroHero } from './IntroHero';
+export { GitHubIcon, LinkedInIcon, DotIcon } from './Icons';
