e25efc1b56d90f7d6a383135cc5f0dd89b8687db
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-04-13 18:07:02 +0200

    Add nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,28 @@
 import * as React from 'react';
 
 export const Nav = () => {
+  const links = [
+    {
+      text: 'Projects',
+      url: '#projects',
+    },
+    {
+      text: 'About',
+      url: '#about',
+    },
+  ];
+
+  const navLinks = links.map(link => (
+    <li>
+      <a href={link.url} className="border-b-2 border-tangerine-500 p-2 mr-2">
+        {link.text}
+      </a>
+    </li>
+  ));
+
   return (
-    <nav className="text-center mt-4">
-      <ul>
-        <li>
-          <a href="#">Projects</a>
-        </li>
-      </ul>
+    <nav className="text-center">
+      <ul className="flex">{navLinks}</ul>
     </nav>
   );
 };
