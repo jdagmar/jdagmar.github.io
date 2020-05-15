8ee189d58ef850515d0fe1eb4b5f85ca2e738192
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-05-15 18:48:03 +0200

    New design for projectcards

--- src/components/ProjectCards.tsx
+++ src/components/ProjectCards.tsx
@@ -1,51 +1,49 @@
 import * as React from 'react';
 import projects from './../data/projects.json';
 import { iconHelper } from './../helpers/iconHelper';
-import { IconLink } from './IconLink';
+import { Link } from './Link';
 
 export const ProjectCards = () => {
   const cards = projects.map(project => (
-    <li className="w-full  py-1 pr-2 last:pr-0 md:w-1/3" key={project.name}>
-      <div className="shadow rounded bg-white py-2 px-4 flex flex-col justify-between h-full">
-        <div className="">
-          <h3 className="font-montserrat text-xl font-black">{project.name}</h3>
+    <li className="w-full my-4 p-8 bg-gray-200" key={project.name}>
+      <div className="flex justify-between flex-col lg:flex-row">
+        <div className="flex flex-col justify-between">
+          <div>
+            <h3 className="uppercase font-montserrat text-2xl font-black">
+              {project.name}
+            </h3>
+            <p className="font-open-sans my-2 max-w-xl">
+              {project.description}
+            </p>
+            <div className="mt-4 mb-2">
+              {project.topics.map((topic, i, topics) => (
+                <>
+                  <span className="font-montserrat text-sm">{topic}</span>
+                  {topics.length - 1 !== i && (
+                    <div className="text-tangerine-500 h-2 w-2 inline-block mx-2">
+                      {iconHelper('dot', 0.5)}
+                    </div>
+                  )}
+                </>
+              ))}
+            </div>
+          </div>
           <div className="flex mt-1 mb-3 text-gray-700">
-            {project.website !== null && (
-              <IconLink
-                icon="globe"
-                iconSize={1}
-                iconPosition="left"
-                url={project.website}
-                text="Try out"
-                isTextVisible={true}
-                cssClass="text-xs mr-2"
-              />
+            {project.url !== null && (
+              <Link url={project.url} text="View Project" />
             )}
-            <IconLink
-              icon="code"
-              iconSize={1}
-              iconPosition="left"
-              url={project.url}
-              text="Source code"
-              isTextVisible={true}
-              cssClass="text-xs"
+            <Link url={project.website} text="View Code" />
+          </div>
+        </div>
+        {project.screenshot.map(screenshot => (
+          <div className="w-full lg:w-1/4">
+            <img
+              src={process.env.PUBLIC_URL + '/' + screenshot}
+              alt=""
+              className="h-auto w-full"
             />
           </div>
-
-          <p className="font-open-sans text-base my-2">{project.description}</p>
-        </div>
-        <div className="mt-4 mb-2">
-          {project.topics.map((topic, i, topics) => (
-            <>
-              <span className="font-montserrat text-xs">{topic}</span>
-              {topics.length - 1 !== i && (
-                <div className="text-tangerine-500 h-2 w-2 inline-block mx-2">
-                  {iconHelper('dot', 0.5)}
-                </div>
-              )}
-            </>
-          ))}
-        </div>
+        ))}
       </div>
     </li>
   ));
--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -4,8 +4,8 @@ import { SectionHeading } from '.';
 
 export const ProjectSection = () => {
   return (
-    <section id="projects" className="md:h-screen  bg-gray-200">
-      <div className="max-w-6xl m-auto py-8 px-4 md:px-0">
+    <section id="projects" className="section relative">
+      <div className="max-w-6xl m-auto py-8 px-4">
         <SectionHeading text="Projects" />
         <ProjectCards />
       </div>
