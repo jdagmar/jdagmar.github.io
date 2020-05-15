9c1d5cf4c55924bf4b78a88c396cc28880495946
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-05-15 18:49:19 +0200

    Uppercase text for sectionheaders

--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const SectionHeading = (props: Props) => {
   return (
     <>
-      <h2 className="font-montserrat text-swamp-green-500 font-black text-2xl">
+      <h2 className="uppercase font-montserrat text-swamp-green-500 font-black text-4xl">
         {props.text}
       </h2>
       <div className="w-8 border-b-2 border-swamp-green-500"></div>
