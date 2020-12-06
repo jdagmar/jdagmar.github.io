649021564f48621a33dd8e54d13fa69756a97d02
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-12-06 21:18:18 +0100

    Minimize logo in mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,13 @@ export const Header = () => {
     <header className="p-4 bg-gray-200 fixed top-0 w-full z-20">
       <div className="max-w-6xl m-auto flex justify-between items-center">
         <div>
-          <p className="font-montserrat text-sm font-black">
-            Jessica Gustavsson
+          <p className="font-montserrat text-sm font-bold">
+            <span className="hidden lg:block">Jessica Gustavsson</span>
+            <span className="text-lg lg:hidden">JG</span>
           </p>
-          <p className="font-open-sans text-xs">Frontend developer</p>
+          <p className="font-open-sans text-xs hidden lg:block">
+            Frontend developer
+          </p>
         </div>
         <Nav />
       </div>
