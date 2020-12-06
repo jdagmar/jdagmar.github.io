9427c536c17e6aced4fb8b6e69f2fbd61c7fccf5
jdagmar <jdagmar@users.noreply.github.com>
2020-12-06 19:44:29 +0000

    deploy: e7b43838a2f289b20f75638ecc860fad79dbe539

---
+++ precache-manifest.e389029a912770dcc296bf34f93fedff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8fcfad2af4f4142a9b3351dd17aae89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d12e01b49f21e057342",
+    "url": "/static/css/main.16d66db1.chunk.css"
+  },
+  {
+    "revision": "81dd7fcf4f6c5242cd3c",
+    "url": "/static/js/2.3cf8e927.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3cf8e927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d12e01b49f21e057342",
+    "url": "/static/js/main.9e4c30c9.chunk.js"
+  },
+  {
+    "revision": "508ccf9f6d7dcca56d13",
+    "url": "/static/js/runtime-main.68d1d5bf.js"
+  },
+  {
+    "revision": "ed8d60393ee9f77f1e57caf70f2dd84a",
+    "url": "/static/media/portrait-github.ed8d6039.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04ccbd033b984e5399394d2e28546e71.js"
+  "/precache-manifest.e389029a912770dcc296bf34f93fedff.js"
 );
 
 self.addEventListener('message', (event) => {
