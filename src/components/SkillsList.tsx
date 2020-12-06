34f2545e749f10b19f7e38979dcb952560def50f
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-12-06 21:18:46 +0100

    Adjust line height

--- src/components/SkillsList.tsx
+++ src/components/SkillsList.tsx
@@ -4,7 +4,7 @@ import { iconHelper } from '../helpers/iconHelper';
 
 export const SkillsList = () => {
   const skillSet = skills.map((skill, i, skills) => (
-    <li className="font-montserrat text-sm" key={i}>
+    <li className="font-montserrat text-sm leading-7" key={i}>
       {skill}
       {skills.length - 1 !== i && (
         <div className="text-tangerine-500 h-2 w-2 inline-block mx-2">
@@ -19,9 +19,7 @@ export const SkillsList = () => {
       <h3 className="uppercase font-montserrat text-xl font-black">
         Technical skills
       </h3>
-      <ul className="flex flex-wrap leading leading-7 text-center my-4">
-        {skillSet}
-      </ul>
+      <ul className="flex flex-wrap text-center my-4">{skillSet}</ul>
     </>
   );
 };
