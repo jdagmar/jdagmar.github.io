73b7c250af4b990eacb4d3860ef59021cdd669ba
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2021-03-01 18:49:28 +0100

    New introhero

--- src/components/IntroHero.tsx
+++ src/components/IntroHero.tsx
@@ -1,15 +1,13 @@
 export const IntroHero = () => {
   return (
-    <div className="bg-gray-200 pt-24">
-      <div className=" py-32 max-w-7xl m-auto flex justify-between px-4 xl:px-0">
-        <div>
-          <h1 className="font-montserrat text-swamp-green-500 text-2xl lg:text-5xl transition motion-reduce:transition-none duration-1000 ease-in-out group-hover:text-tangerine-500 mb-6">
-            Jessica Gustavsson
-          </h1>
-          <p className="font-open-sans tracking-wide">
-            Frontend developer with accessibility in mind
-          </p>
-        </div>
+    <div className="bg-swamp-green-200">
+      <div className=" py-32 max-w-7xl m-auto px-4 lg:px-6 text-center">
+        <h1 className="font-black tracking-wider font-montserrat text-swamp-green-600 text-3xl lg:text-5xl mb-6">
+          Jessica Gustavsson
+        </h1>
+        <p className="font-open-sans mt-6 font-bold text-md lg:text-xl">
+          Frontend developer with accessibility in mind
+        </p>
       </div>
     </div>
   );
