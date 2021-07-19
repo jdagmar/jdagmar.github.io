9230e4595df12522903e451111ba62dbfab32cae
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2021-07-19 21:25:10 +0200

    Wider tracking

--- src/components/IntroHero.tsx
+++ src/components/IntroHero.tsx
@@ -5,7 +5,7 @@ export const IntroHero = () => {
         <h1 className="font-black tracking-wider font-montserrat text-swamp-green-600 text-3xl lg:text-5xl mb-6">
           Jessica Gustavsson
         </h1>
-        <p className="font-open-sans mt-6 font-bold text-md lg:text-xl">
+        <p className="font-open-sans tracking-wide mt-6 font-bold text-md lg:text-xl">
           Frontend developer with{' '}
           <span className="custom-underline custom-underline-tangerine relative">
             accessibility
