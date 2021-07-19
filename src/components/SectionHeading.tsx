b08bbd6fa910b3ebda7e19896789088f65c57aa2
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2021-07-19 21:25:31 +0200

    Sectionheadings should be uppercase

--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const SectionHeading = (props: Props) => {
   return (
     <>
-      <h2 className="tracking-wide font-montserrat text-swamp-green-600 font-black text-4xl lg:text-5xl pb-3">
+      <h2 className="tracking-wide font-montserrat text-swamp-green-600 font-black text-4xl lg:text-5xl pb-3 uppercase">
         {props.text}
       </h2>
       <div className="w-20 border-b-4 border-swamp-green-600 rounded"></div>
