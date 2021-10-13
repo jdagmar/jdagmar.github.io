992a8b3ca2bee34952ba0145ec19a51b9d270981
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2021-10-13 22:35:15 +0200

    Change portrait image

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import portrait from './../images/portrait-github.png';
+import portrait from './../images/portrait.jpeg';
 import { SkillsList } from './SkillsList';
 import { SectionHeading, Link } from '.';
 
@@ -9,20 +8,21 @@ export const AboutSection = () => {
       <div className="bg-swamp-green-200">
         <div className="max-w-7xl m-auto pb-20 pt-24 px-4 lg:px-10">
           <SectionHeading text="About" />
-          <div className="flex justify-between lg:flex-row flex-col-reverse mt-20 w-full my-4">
+          <div className="flex justify-between lg:flex-row flex-col-reverse mt-10 w-full my-4">
             <div className="max-w-3xl">
               <h2 className="tracking-wide font-maven-pro text-3xl font-black">
-                Who I am
+                Profile
               </h2>
               <p className="text-md font-open-sans mt-4 mb-16 leading-7">
-                I'm a frontend developer based in Stockholm. I build websites
-                for Episerver and Umbraco by day and apps in angular/react by
-                night.
+                I am a fullstack developer based in Stockholm, currently as an
+                consultant. I have extensive experience writing WCAG compliant
+                code, testing and reviewing codebases from an accessibility
+                standpoint.
               </p>
               <SkillsList />
               <div className="mt-16">
                 <h2 className="tracking-wide font-maven-pro text-3xl font-black mb-5">
-                  Where you can find me
+                  Networking
                 </h2>
                 <Link
                   url="https://www.linkedin.com/in/jessica-gustavsson-96375515b/"
